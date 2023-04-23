ff8ddc03ff34700d3166f45b3ff910875cd92246
Luis Ocana - Apdayc <luis.ocana@apdayc.org.pe>
2023-04-22 23:36:56 -0500

    Se anadio la pantalla de creacion del formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatTabsModule} from '@angular/material/tabs';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { EncuestaComponent } from './components/encuesta/encuesta.component';
 import { Preg01Component } from './components/preg01/preg01.component';
 import { Preg02Component } from './components/preg02/preg02.component';
 import { Preg03Component } from './components/preg03/preg03.component';
 import { Preg04Component } from './components/preg04/preg04.component';
 import { HomeComponent } from './components/home/home.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { EncuestaComponent } from './components/encuesta/encuesta.component';
 
 
 const appRoutes: Routes = [
+  {path: 'appencuesta', component: EncuestaComponent},
   {path: '', component: HomeComponent,pathMatch:"full"},
   {path: 'principal', component: PrincipalComponent},
   {path: 'preg01', component: Preg01Component},
   {path: 'preg02', component: Preg02Component},
   {path: 'preg03', component: Preg03Component},
   {path: 'preg04', component: Preg04Component},
+  
 
 ];
 
@@ -33,15 +35,14 @@ const appRoutes: Routes = [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    EncuestaComponent,
-    EncuestaComponent,
-    EncuestaComponent,
     Preg01Component,
     Preg02Component,
     Preg03Component,
     Preg04Component,
     HomeComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    EncuestaComponent
+    
   ],
   imports: [
     BrowserModule,
@@ -49,7 +50,8 @@ const appRoutes: Routes = [
     FormsModule,
     RouterModule.forRoot(appRoutes),
     MatTabsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
